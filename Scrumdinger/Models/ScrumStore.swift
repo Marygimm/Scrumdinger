0355d6ee5ef0ba881e21cd8594050acd543ae39f
Marygimm <marygimm.moreira@bringglobal.com>
2022-10-03 11:21:08 +0000

    Create a data store

---
+++ Scrumdinger/Models/ScrumStore.swift
@@ -0,0 +1,22 @@
+//
+//  ScrumStore.swift
+//  Scrumdinger
+//
+//  Created by Mary Moreira on 03/10/2022.
+//
+
+import Foundation
+import SwiftUI
+
+class ScrumStore: ObservableObject {
+    @Published var scrums: [DailyScrum] = []
+    
+    private static func fileURL() throws -> URL {
+        try FileManager.default.url(for: .documentDirectory,
+                                    in: .userDomainMask,
+                                    appropriateFor: nil,
+                                    create: false)
+        .appendingPathComponent("scrums.data")
+        
+    }
+}
