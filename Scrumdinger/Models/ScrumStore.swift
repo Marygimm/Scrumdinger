0c3a96a43aa7dc1cf0f8aa12fa894737097f90fb
Marygimm <marygimm.moreira@bringglobal.com>
2022-10-03 11:23:36 +0000

    Add a method to save data

--- Scrumdinger/Models/ScrumStore.swift
+++ Scrumdinger/Models/ScrumStore.swift
@@ -21,24 +21,41 @@ class ScrumStore: ObservableObject {
     }
     
     static func load(completion: @escaping (Result<[DailyScrum], Error>)->Void) {
-          DispatchQueue.global(qos: .background).async {
-              do {
-                  let fileURL = try fileURL()
-                  guard let file = try? FileHandle(forReadingFrom: fileURL) else {
-                      DispatchQueue.main.async {
-                          completion(.success([]))
-                      }
-                      return
-                  }
-                  let dailyScrums = try JSONDecoder().decode([DailyScrum].self, from: file.availableData)
-                  DispatchQueue.main.async {
-                      completion(.success(dailyScrums))
-                  }
-              } catch {
-                  DispatchQueue.main.async {
-                      completion(.failure(error))
-                  }
-              }
-          }
-      }
+        DispatchQueue.global(qos: .background).async {
+            do {
+                let fileURL = try fileURL()
+                guard let file = try? FileHandle(forReadingFrom: fileURL) else {
+                    DispatchQueue.main.async {
+                        completion(.success([]))
+                    }
+                    return
+                }
+                let dailyScrums = try JSONDecoder().decode([DailyScrum].self, from: file.availableData)
+                DispatchQueue.main.async {
+                    completion(.success(dailyScrums))
+                }
+            } catch {
+                DispatchQueue.main.async {
+                    completion(.failure(error))
+                }
+            }
+        }
+    }
+    
+    static func save(scrums: [DailyScrum], completion: @escaping (Result<Int, Error>)->Void) {
+        DispatchQueue.global(qos: .background).async {
+            do {
+                let data = try JSONEncoder().encode(scrums)
+                let outfile = try fileURL()
+                try data.write(to: outfile)
+                DispatchQueue.main.async {
+                    completion(.success(scrums.count))
+                }
+            } catch {
+                DispatchQueue.main.async {
+                    completion(.failure(error))
+                }
+            }
+        }
+    }
 }
