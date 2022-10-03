9ecc3257b31ea39b35de15cf97b5a5493870414f
Marygimm <marygimm.moreira@bringglobal.com>
2022-10-03 11:48:54 +0000

    Create an error view

---
+++ Scrumdinger/Views/ErrorView.swift
@@ -0,0 +1,45 @@
+//
+//  ErrorView.swift
+//  Scrumdinger
+//
+//  Created by Mary Moreira on 03/10/2022.
+//
+
+import SwiftUI
+
+struct ErrorView: View {
+    let errorWrapper: ErrorWrapper
+    
+    var body: some View {
+        VStack {
+            Text("An error has occurred!")
+                .font(.title)
+                .padding(.bottom)
+            Text(errorWrapper.error.localizedDescription)
+                .font(.headline)
+            Text(errorWrapper.guidance)
+                .font(.caption)
+                .padding(.top)
+            Spacer()
+        }
+        .padding()
+        .background(.ultraThinMaterial)
+        .cornerRadius(16)
+    }
+}
+
+struct ErrorView_Previews: PreviewProvider {
+    
+    enum SampleError: Error {
+        case errorRequired
+    }
+    
+    static var wrapper: ErrorWrapper {
+        ErrorWrapper(error: SampleError.errorRequired,
+                     guidance: "You can safely ignore this error.")
+    }
+    
+    static var previews: some View {
+        ErrorView(errorWrapper: wrapper)
+    }
+}
